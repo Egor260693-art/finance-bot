dc807808994f233b9cc1214d6453f10c1b09970c
Egor260693-art <Egor260693@icloud.com>
2026-08-01 21:54:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # ТОКЕН БОТА (ЗАМЕНИТЕ НА ВАШ НОВЫЙ ТОКЕН!)
 # СРОЧНО: отзовите старый через @BotFather -> /revoke
 # =============================================
-TOKEN = "AAHR0xBjhUlfH8V8-iIcDABcflBCPcB74CE"
+TOKEN = "8805553209:AAHR0xBjhUlfH8V8-iIcDABcflBCPcB74CE"
 
 # ВАЖНО: threaded=False для работы с webhook!
 bot = telebot.TeleBot(TOKEN, threaded=False)
